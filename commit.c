fee7d947d9be313b2c2adebaf3361ab27f4f3875
Aditya <aditya@Adityas-MacBook-Air-2.local>
2026-04-16 18:20:12 +0530

    Implement commit creation

--- commit.c
+++ commit.c
@@ -194,8 +194,36 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
-    return -1;
+    ObjectID tree_id;
+    if (tree_from_index(&tree_id) != 0) return -1;
+
+    Commit commit;
+    memset(&commit, 0, sizeof(commit));
+    commit.tree = tree_id;
+    commit.has_parent = 0;
+
+    if (head_read(&commit.parent) == 0) {
+        commit.has_parent = 1;
+    }
+
+    snprintf(commit.author, sizeof(commit.author), "%s", pes_author());
+    commit.timestamp = (uint64_t)time(NULL);
+    snprintf(commit.message, sizeof(commit.message), "%s", message);
+
+    void *data = NULL;
+    size_t len = 0;
+    if (commit_serialize(&commit, &data, &len) != 0) return -1;
+
+    ObjectID new_commit;
+    int rc = object_write(OBJ_COMMIT, data, len, &new_commit);
+    free(data);
+    if (rc != 0) return -1;
+
+    if (head_update(&new_commit) != 0) return -1;
+
+    if (commit_id_out) {
+        *commit_id_out = new_commit;
+    }
+
+    return 0;
 }
